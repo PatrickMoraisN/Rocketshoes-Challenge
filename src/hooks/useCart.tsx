e0368c6554940a1ffe8d195f45f4287741f74486
Patrick Morais <ppternunes@gmail.com>
2021-08-16 23:40:17 -0300

    att amount / setCart product

--- src/hooks/useCart.tsx
+++ src/hooks/useCart.tsx
@@ -41,6 +41,25 @@ export function CartProvider({ children }: CartProviderProps): JSX.Element {
       const stockAmount = stock.data.amount;
       const currentAmount = productExists?.amount ?? 0;
       const amount = currentAmount + 1;
+
+      if (amount > stockAmount) {
+        toast.error('Quantidade solicitada fora de estoque');
+        return;
+      }
+
+      if (productExists) {
+        productExists.amount = amount;
+      } else {
+        const product = await api.get(`products/${productId}`);
+
+        const newProduct = {
+          ...product.data,
+          amount: 1,
+        }
+        updatedCart.push(newProduct);
+      }
+      setCart(updatedCart);
+      localStorage.setItem('@RocketShoes:cart', JSON.stringify(updatedCart))
     } catch (error) {
       toast.error('Erro na adição do produto');
     }
